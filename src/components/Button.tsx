0d5afc870ffaabe1d45344c90d837d13dbf7407f
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-26 14:59:27 +0100

    ref(design): style Buttom using styled-components
    Ref: #3

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,33 @@
+import { styled } from "styled-components";
+
 type Props = {
   children: React.ReactNode;
   onClick: () => void;
 };
 
+const StyleButton = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #f9f9f9;
+  cursor: pointer;
+  transition: border-color 0.25s;
+
+  &:hover {
+    border-color: #646cff;
+  }
+
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+`;
+
 function Button({ children, onClick }: Props) {
-  return <button onClick={onClick}>{children}</button>;
+  return <StyleButton onClick={onClick}>{children}</StyleButton>;
 }
 
 export { Button };
