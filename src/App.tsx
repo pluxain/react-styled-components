48dea956bdd678dc1da19d35ea3e7f17497fc419
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-26 15:08:24 +0100

    ref(design): style Counter wrapper using styled-components
    Ref: #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { styled } from "styled-components";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
 import { Card, DocBlock, Image, Link, Title } from "./components";
 import { Counter } from "./features/counter";
 import { StyledLink } from "./components/Link";
+
+const CounterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 1rem;
+`;
 
 function App() {
   return (
@@ -21,10 +29,10 @@ function App() {
         <StyledLink to="https://react.dev">React</StyledLink>
       </Title>
       <Card>
-        <div className="flex justify-around items-center gap-4">
+        <CounterWrapper>
           <Counter start={1} />
           <Counter start={0} step={2} />
-        </div>
+        </CounterWrapper>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
