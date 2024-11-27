36c40aa8a37a5de7449780453104d3cf6289729a
Pascal Luxain <pascal.luxain@gmail.com>
2024-11-27 12:20:38 +0100

    feat(design): add legend to Counter
    Ref: #5

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 import { Button } from "../../components";
+import styled from "styled-components";
 
 type Props = {
   start: number;
   step?: number;
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
 function Counter({ start, step = 1 }: Props) {
   const [count, setCount] = useState(start);
   return (
-    <Button $primary onClick={() => setCount((count) => count + step)}>
-      count is {count}
-    </Button>
+    <Wrapper>
+      <Button $primary onClick={() => setCount((count) => count + step)}>
+        count is {count}
+      </Button>
+      <small>
+        start at {start} and has a {step} increment
+      </small>
+    </Wrapper>
   );
 }
 
